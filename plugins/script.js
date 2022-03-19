9d674281852cbd46a50b7ccabf0f2da29547e779
ryoromance <100901480+NayBot12@users.noreply.github.com>
2022-03-19 15:02:29 +0700

    Update script.js

--- plugins/script.js
+++ plugins/script.js
@@ -1,7 +1,7 @@
 let handler  = async (m, { conn, usedPrefix }) => {
   conn.reply(m.chat, `
-*Lord Botz Menggunakan Script*
-github.com/AmmarBN/lordbot-aq-tes
+*Riyo Botz Menggunakan Script*
+https://github.com/RiyoXd/lordbot-v2
 _*Jangan Lupa Follow Sama Kasi Star Ya kak*_
 
 `.trim(), m)
